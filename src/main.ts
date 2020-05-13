924de4de140c7f6d1ee3024841794b0e6c90c7e1
Orlando <osierra@level6.co>
2020-05-13 12:31:04 -0400

    Modifying  src/main.ts to use a dynamic port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {AppModule} from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({origin: '*'});
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
